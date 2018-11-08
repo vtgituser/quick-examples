75fbc807446009970a160b84e8f6b7c5a6c581b0
Vasu Telaprolu <Vasubabu.Telaprolu@aexp.com>
2018-11-08 13:36:15 -0700

    byte stream example

---
+++ src/main/java/com/vt/ByteStream.java
@@ -0,0 +1,12 @@
+package com.vt;
+
+import java.util.stream.Stream;
+
+public class ByteStream {
+  public static void main(String[] args) {
+    Byte[] data = new Byte[] {65, 66, 67};
+    Byte first = Stream.of(data).findFirst().get();
+    System.out.println(first);
+
+  }
+}
